a5d45c3f89c707ca3329e1571fc5cf5b2c807788
AchieversIT <achieversitsol@gmail.com>
2021-11-16 10:30:38 +0530

    operators

---
+++ javascript/script.js
@@ -0,0 +1,61 @@
+// console.log("Hello world");
+
+// Operators
+// Arthemetic Operator
+//  +,-,*, %,/, **
+// compararsion operator
+// ==,===, !=
+// logical operator
+// assignment operator
+// ternary operator
+// increment / decrement operator
+// condtion statement
+
+console.log(2 + 2);
+console.log(10 - 2);
+console.log(10 * 2);
+
+// modulus % (which gives us the reminder )
+
+console.log(10 % 2);
+//  division / (which give quoutient)
+console.log(10 / 2);
+
+console.log(2 ** 10);
+// Discount = (Listing - selling)/Listing * 100
+// BODAMS
+var discount = ((3695 - 1476) / 3695) * 100;
+console.log(discount);
+
+var email = "example@gmail.com";
+var password = "123456";
+
+// console.log(email == "example@gmail.com");
+
+// What is Difference between == and ===
+//  == will check the value and ignores the data type
+// === (strict equalto) will check the value and data type
+console.log("1" == 1);
+console.log("1" === 1);
+
+// Assignment Operators
+// =,+=,-=,*=,%=,
+
+var total = 0;
+// i buyed a milk product
+// total = total + 30;
+total += 30;
+// i buyed a maggie
+total += 10;
+console.log(total);
+
+// condtional statements are like
+// based on condtion if you want to execute certain code
+
+var myNumber = 10;
+
+if (myNumber % 2 == 0) {
+  console.log("It is even");
+} else {
+  console.log("It is Odd");
+}
